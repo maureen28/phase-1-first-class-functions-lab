698f6b49b67f5b9bb6c62bb910cee2e1712a9b93
maureen <maureen.murimi@moringaschool.com>
2022-05-09 23:20:48 +0300

    add callback and arrow functions

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+// // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+let returnFirstTwoDrivers = drivers => {
+    return drivers.slice(0,2)
+};
+
+let returnLastTwoDrivers = (drivers) =>{
+    return drivers.slice(drivers.length -2)
+};
+
+let selectingDrivers =[
+    returnFirstTwoDrivers, 
+    returnLastTwoDrivers
+];
+
+let createFareMultiplier = multiplier => {
+    return function(n){
+        return n * multiplier
+    } 
+}
+function fareDoubler(createFareMultiplier){
+    return createFareMultiplier * 2
+}
+function fareTripler(createFareMultiplier){
+    return createFareMultiplier * 3
+}
+
+let selectDifferentDrivers = (arrayOfDrivers, selectedDrivers) => {
+	return selectedDrivers(arrayOfDrivers)
+}
+
+
+
